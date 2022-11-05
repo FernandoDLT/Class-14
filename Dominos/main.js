b2715be67a648b0de953456f249113845233e37f
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-11-05 15:40:28 -0500

    temp converter

---
+++ Dominos/main.js
@@ -0,0 +1,8 @@
+const pizza = 'Dominos'
+    if(pizza === 'Papa Johns') {
+        console.log('Scram!')
+    }else if(pizza === 'Dominos') {
+        console.log('All aboard the flavor train!')
+    }else{
+        console.log('No pizza for you!')
+    }
---
+++ simple-functions/main.js
@@ -0,0 +1,58 @@
+//create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1,num2) {
+    alert( num1 + num2)
+}
+
+addTwoNumbers(20,30)
+//create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(n1,n2,n3) {
+    const product = n1 * n2 * n3
+    console.log(product)
+}
+multiplyThreeNumbers(2,3,4)
+
+//create a function that divides two numbers and returns the ???
+
+function dividesTwoAndReturns(nn1,nn2) {
+    return nn1 /nn2
+}
+
+console.log(dividesTwoAndReturns(12,6))
+
+// Variables
+// Create a variable and console.log the value
+
+let favCar = 'ferrari'
+console.log(favCar)
+
+// Create a variable, add 10 to it and alert the value
+
+let x = 5
+x += 5 
+alert(x)
+
+// Functions
+// Create a function that subtracts 4 numbers and alerts the difference
+
+function subFourNums(cat1,cat2,cat3,cat4) {
+    alert( cat1 - cat2 - cat3 - cat4 )
+}
+subFourNums(100,5,20,40)
+
+// Create a function that divides one number by another and returns the remainder
+
+function divTwoNums(one,two) {
+    alert(one / two)
+}
+
+divTwoNums(10,5)
+console.log(divTwoNums)
+
+// Conditionals
+// Create a function that adds two numbers and if the  is greater than 50 alert Jumanji
+
+
+// Create a function that multiplies three numbers and if the product is divisible by 3 alert Zebra
+
+
+
