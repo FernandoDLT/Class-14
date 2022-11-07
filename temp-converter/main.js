6ad7c6ad8d893c0907cea0ead93a282b1d2f0d5e
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-11-07 12:13:27 -0600

    final rose javascript

--- temp-converter/main.js
+++ temp-converter/main.js
@@ -1,17 +1,15 @@
 // Write pseudo code first
+
+
 // Only on "click" 0 -> 32
-// Get the value out of input
-// Convert the value
-// Show the value/result to the user
-
-
 document.querySelector('#convert').addEventListener('click', convert)
 
-    function convert() {
-        let temp = document.querySelector('#cel').value
-
-        temp = temp * 9/5 + 32
-
-        document.querySelector('#placeToYell').innerText = temp
+function convert() {
+    // Get the value out of input
+    let temp = document.querySelector('#cel').value
+    // Convert the value
+    temp = temp * 9/5 + 32
+    // Show the value/result to the user
+    document.querySelector('#placeToYell').innerText = temp
     }
 
