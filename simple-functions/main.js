caa39aecbaec0f58ddf69fb23c73ef6935dff716
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-11-07 14:34:59 -0600

    practice using function calls

--- simple-functions/main.js
+++ simple-functions/main.js
@@ -1,52 +1,52 @@
 //create a function that adds two numbers and alerts the sum
-function addTwoNumbers(num1,num2) {
-    alert( num1 + num2)
-}
+// function addTwoNumbers(num1,num2) {
+//     alert( num1 + num2)
+// }
 
-addTwoNumbers(20,30)
+// addTwoNumbers(20,30)
 //create a function that multiplys three numbers and console logs the product
-function multiplyThreeNumbers(n1,n2,n3) {
-    const product = n1 * n2 * n3
-    console.log(product)
-}
-multiplyThreeNumbers(2,3,4)
+// function multiplyThreeNumbers(n1,n2,n3) {
+//     const product = n1 * n2 * n3
+//     console.log(product)
+// }
+// multiplyThreeNumbers(2,3,4)
 
 //create a function that divides two numbers and returns the ???
 
-function dividesTwoAndReturns(nn1,nn2) {
-    return nn1 /nn2
-}
+// function dividesTwoAndReturns(nn1,nn2) {
+//     return nn1 /nn2
+// }
 
-console.log(dividesTwoAndReturns(12,6))
+// console.log(dividesTwoAndReturns(12,6))
 
 // Variables
 // Create a variable and console.log the value
 
-let favCar = 'ferrari'
-console.log(favCar)
+// let favCar = 'ferrari'
+// console.log(favCar)
 
 // Create a variable, add 10 to it and alert the value
 
-let x = 5
-x += 5 
-alert(x)
+// let x = 5
+// x += 5 
+// alert(x)
 
 // Functions
 // Create a function that subtracts 4 numbers and alerts the difference
 
-function subFourNums(cat1,cat2,cat3,cat4) {
-    alert( cat1 - cat2 - cat3 - cat4 )
-}
-subFourNums(100,5,20,40)
+// function subFourNums(cat1,cat2,cat3,cat4) {
+//     alert( cat1 - cat2 - cat3 - cat4 )
+// }
+// subFourNums(100,5,20,40)
 
 // Create a function that divides one number by another and returns the remainder
 
-function divTwoNums(one,two) {
-    alert(one / two)
-}
+// function divTwoNums(one,two) {
+//     alert(one / two)
+// }
 
-divTwoNums(10,5)
-console.log(divTwoNums)
+// divTwoNums(10,5)
+// console.log(divTwoNums)
 
 // Conditionals
 // Create a function that adds two numbers and if the  is greater than 50 alert Jumanji
@@ -55,4 +55,21 @@ console.log(divTwoNums)
 // Create a function that multiplies three numbers and if the product is divisible by 3 alert Zebra
 
 
+//create a function that adds two numbers and alerts the sum
 
+function addTwo (numOne, numTwo) {
+    alert(numOne + numTwo)
+}
+addTwo(1, 30)
+
+// Create a function that multis 3 numbers and console logs the product
+
+function multiThreeNums (nOne, nTwo, nThree) {
+    alert(nOne * nTwo * nThree)
+}
+multiThreeNums(2, 4, 3)
+
+function divTwoRetRem(numbOne, numbTwo) {
+    alert(numbOne % numbTwo)
+}
+divTwoRetRem(10, 3)
